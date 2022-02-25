f5f3086843b9e531b61685f5828cfc879e211bc8
Will Sams <will@samswebs.com>
2022-02-25 08:44:42 -0500

    Changes conventions for models, adds soccerTableName var.

--- backend/src/utils/server/index.js
+++ backend/src/utils/server/index.js
@@ -1,7 +1,10 @@
 const isDevelopmentHost = () =>
   process.env.NODE_ENV === 'development' ? true : false;
 
+const soccerTableName =
+  process?.env?.NODE_ENV ? `Deportivo-${process?.env?.NODE_ENV}` : process?.env?.SOCCER_TABLE;
 
 module.exports = {
   isDevelopmentHost,
+  soccerTableName,
 };
